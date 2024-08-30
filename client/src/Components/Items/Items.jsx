01c785e9cf04eb5af8ef710c219456d1bb5f5f9a
Jadaunshiv <163510201+Jadaunshiv@users.noreply.github.com>
2024-08-30 22:38:10 +0530

    Update Items.jsx

--- client/src/Components/Items/Items.jsx
+++ client/src/Components/Items/Items.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 const Items = (props) => {
   return (
     <div className='items'>
-        <Link to={`/product/${props.id}`}><img onClick={window.scroll(0, 0)} src={props.image} alt=""/></Link>
-        <p>{props.name}</p>
+        <Link to={`/product/${props.id}`}><img onClick={window.scroll(0, 0)} src={props.image} alt=""/><p>{props.name}</p></Link>
+        
         <div className="items-price">
             <div className="discounted-price">
                 ${props.discounted_price}
