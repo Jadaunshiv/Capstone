360ea2a9652834810ef29c5a71f49432a720129c
Jadaunshiv <163510201+Jadaunshiv@users.noreply.github.com>
2024-08-30 16:42:10 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch('http://localhost:10000/allproducts')
+    await fetch('https://simple-goods-server.onrender.com')
     .then((res) => res.json())
     .then((data) => {setAllProducts(data)});
   }
@@ -17,7 +17,7 @@ const ListProduct = () => {
   },[])
 
   const remove_product = async (id) => {
-    await fetch('http://localhost:10000/removeproduct', {
+    await fetch('https://simple-goods-server.onrender.com', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
